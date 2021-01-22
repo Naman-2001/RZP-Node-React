5f41bd5f89e1c6a0bff02951de0b9ef244367cf3
unknown <naman.aggarwal2001@gmail.com>
2021-01-22 20:20:11 +0530

    completed

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,8 @@ const path = require("path");
 const shortid = require("shortid");
 const Razorpay = require("razorpay");
 const cors = require("cors");
+const boyParser = require("body-parser");
+const bodyParser = require("body-parser");
 
 const razorpay = new Razorpay({
   key_id: "rzp_test_tPppRmOaMAbGwD",
@@ -10,9 +12,28 @@ const razorpay = new Razorpay({
 });
 
 app.use(cors());
+app.use(bodyParser.json());
 
 app.get("/logo.svg", (req, res) => {
   res.sendFile(path.join(__dirname, "logo.svg"));
+});
+
+app.post("/verification", (req, res) => {
+  const secret = "Naman076";
+  console.log(req.body);
+
+  const crypto = require("crypto");
+  const shasum = crypto.createHmac("sha256", secret);
+  shasum.update(JSON.stringify(req.body));
+  const digest = shasum.digest("hex");
+  console.log(digest, req.headers["x-razorpay-signature"]);
+  if (digest === req.headers["x-razorpay-signature"]) {
+    console.log("Legit payment");
+    res.json({ status: "ok" });
+  } else {
+    res.status(502).json({ message: "req. not legit dear" });
+    console.log("|nhi bhai galat hai");
+  }
 });
 
 app.post("/razorpay", async (req, res) => {
